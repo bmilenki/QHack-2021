5c0916b755c8937398130e94d434ec0cb80b137e
unknown <bmilenki@gmail.com>
2021-02-19 18:29:43 -0500

    Working

--- Q5/Q5.py
+++ Q5/Q5.py
@@ -42,7 +42,75 @@ def gradient_200(weights, dev):
     hessian = np.zeros([5, 5], dtype=np.float64)
 
     # QHACK #
+    def parameter_shift_term(qnode, params, i, shift):
+        shifted = params.copy()
+        shifted[i] += shift
+        forward = qnode(shifted)  # forward evaluation
 
+        shifted[i] -= shift * 2
+        backward = qnode(shifted)  # backward evaluation
+
+        return [((1/np.sin(shift))*.5 * (forward - backward)), [forward, backward]]
+
+    def parameter_shift(qnode, params, shift):
+        gradients = np.zeros([len(params)])
+        devEval = []
+
+        for i in range(len(params)):
+            ans = parameter_shift_term(qnode, params, i, shift)
+            gradients[i] = ans[0]
+            devEval.append(ans[1])
+
+        return gradients, devEval
+
+    def hessianCalc(devEval,qnode,params,shift):
+        f = qnode(params)
+        shiftedAnswers = np.zeros([5, 5], dtype=np.float64)
+        for i in range(len(params)):
+            for j in range(i+1,len(params)):
+                weights = params.copy()
+                weights[i] = weights[i] + shift
+                weights[j] = weights[j] + shift
+                s1 = qnode(weights)
+
+                weights = params.copy()
+                weights[i] = weights[i] - shift
+                weights[j] = weights[j] + shift
+                s2 = qnode(weights)
+
+                weights = params.copy()
+                weights[i] = weights[i] + shift
+                weights[j] = weights[j] - shift
+                s3 = qnode(weights)
+
+                weights = params.copy()
+                weights[i] = weights[i] - shift
+                weights[j] = weights[j] - shift
+                s4 = qnode(weights)
+
+                # print(s1, s2, s3, s4)
+                # print("answer:",(s1-s2-s3+s4)/((2*np.sin(shift))**2))
+
+                shiftedAnswers[i][j] = (s1-s2-s3+s4)/((2*np.sin(shift))**2)
+                shiftedAnswers[j][i] = shiftedAnswers[i][j]
+
+        # calc diagonal using gradients/shifted answers
+        for k in range(len(params)):
+            forward = devEval[k][0]
+            backward = devEval[k][1]
+
+            shiftedAnswers[k][k] = (forward - 2*f + backward)/2
+
+        #print(shiftedAnswers)
+        return shiftedAnswers
+
+    gradient, devEval = parameter_shift(circuit, weights, np.pi / 2)
+
+    hessian = hessianCalc(devEval,circuit, weights, np.pi / 2)
+
+    #print(gradient[0] * parameter_shift(circuit, gradient) + )
+
+    #hessian = hessian(gradient,circuit,weights)
     # QHACK #
 
     return gradient, hessian, circuit.diff_options["method"]
